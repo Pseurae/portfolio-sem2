c44b262cfebbdd0c4d049fcffb0131aade7f691d
Pseurae <adhith.chand@gmail.com>
2024-02-05 17:52:50 +0530

    Add mouse trailer

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+const lenis = new Lenis();
+
+function createLoadingBars(num)
+{
+    var container = document.createElement('div');
+    container.classList.add('bars');
+
+    var widthPercent = (100 / num);
+
+    for (let i = 0; i < num; ++i)
+    {
+        var bar = document.createElement('div');
+        bar.classList.add('bar');
+        bar.style.width = widthPercent + '%';
+
+        container.appendChild(bar);
+    }
+
+    document.body.appendChild(container);
+}
+
+function createTrailer()
+{
+    const trailer = document.querySelector('.trailer');
+
+    const animateTrailer = (e, interacting) => {
+        const x = e.clientX - trailer.offsetWidth / 2, 
+              y = e.clientY - trailer.offsetHeight / 2;
+
+        const keyframes = {
+            left: x + 'px',
+            top: y + 'px',
+            scale: (interacting ? 3.0 : 1.0)
+        };
+
+        trailer.animate(keyframes, { duration: 200, fill: "forwards" });
+    };
+
+    window.addEventListener('mousemove', (e) => {
+        const on_link = e.target.closest('a');
+        animateTrailer(e, on_link !== null);
+    });
+}
+
 function applyPortraitEffects()
 {
     var img = document.querySelector(".portrait");
@@ -16,7 +60,6 @@ function applyPortraitEffects()
 
 function applyLenisScroll()
 {
-    const lenis = new Lenis();   
     lenis.on('scroll', ScrollTrigger.update);
     gsap.ticker.add((time)=>{
       lenis.raf(time * 1000)
@@ -26,11 +69,7 @@ function applyLenisScroll()
 
 function scrollToTop()
 {
-    window.scroll({
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth' 
-    });
+    lenis.scrollTo(0, { duration: 3, easing: (x) => { return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2; } });
 }
 
 function applyStartingAnimation()
@@ -45,17 +84,17 @@ function applyStartingAnimation()
         },
     );
 
-    const splitText = new SplitType(".hero .name h1");
-    gsap.from(
-        splitText.chars, 1.5,
-        {
-            y: 300,
-            opacity: 0,
-            delay: 1.5,
-            stagger: { amount: 0.2 },
-            ease: 'power4.inOut'
-        }
-    )
+    // const splitText = new SplitType(".hero .name h1");
+    // gsap.from(
+    //     splitText.chars, 1.5,
+    //     {
+    //         y: 300,
+    //         opacity: 0,
+    //         delay: 1.5,
+    //         stagger: { amount: 0.2 },
+    //         ease: 'power4.inOut'
+    //     }
+    // );
 
     gsap.from(
         '.hero .name p', 1.5,
@@ -64,72 +103,128 @@ function applyStartingAnimation()
             delay: 2.15,
             ease: 'power4.inOut'
         }
-    )
+    );
+
+    gsap.from(
+        '.hero .circles', 1.5,
+        {
+            scale: 0,
+            delay: 1.5,
+            ease: 'power4.out'
+        }
+    );
 }
 
 function applyScrollTriggers()
 {
+    // Hero Section
     gsap.to(
-        ".scroll-indicator",
+        '.hero',
         {
             scrollTrigger: {
-                trigger: ".hero",
-                start: "bottom 50%",
-                end: "bottom 70%",
-                scrub: 1
+                trigger: '.hero',
+                start: 'bottom 100%',
+                end: 'bottom 50%',
+                scrub: 1,
+            },
+            opacity: 0,
+            ease: "power2.inOut"
+        }
+    );
+
+    // Educational Qualification
+    gsap.from(
+        '.section.education .container .listing', 1.0,
+        {
+            scrollTrigger: {
+                trigger: '.section.education .section-header',
+                start: 'top 60%',
+            },
+            x: 100,
+            opacity: 0,
+            stagger: { amount: 0.5 },
+            ease: 'power4.inOut'
+        },
+    );
+
+    gsap.from(
+        '.section.education .container .line', 2.0,
+        {
+            scrollTrigger: {
+                trigger: '.section.education .section-header',
+                start: 'top 40%',
+            },
+            '--height': '0%',
+            ease: 'power3.inOut'
+        },
+    );
+
+    // About Me
+    gsap.from(
+        '.section.about-me .section-header', 1.0,
+        {
+            scrollTrigger: {
+                trigger: '.section.about-me .section-header',
+                start: 'top 70%',
             },
             opacity: 0
+        }
+    );
+
+    gsap.from(
+        '.section.about-me .container', 1.5,
+        {
+            scrollTrigger: {
+                trigger: '.section.about-me .section-header',
+                start: 'top 60%',
+            },
+            opacity: 0,
+            y: 100,
+            ease: 'power4.inOut'
         }
     )
 
-    gsap.from(
-        '.section.education .section-header', 0.5,
-        {
-            scrollTrigger: {
-                trigger: '.section.education .section-header',
-                start: 'top 80%'
-            },
-            opacity: 0
-        }
-    );
+    // gsap.from(
+    //     '.section.about-me .container .portrait-container', 1.0,
+    //     {
+    //         scrollTrigger: {
+    //             trigger: '.section.about-me .section-header',
+    //             start: 'top 60%',
+    //         },
+    //         opacity: 0,
+    //         x: 100
+    //     }
+    // )
 
-    gsap.from(
-        '.section.education .listing', 1.0,
+    // Made with
+    var madeWithTimeline = gsap.timeline({ scrollTrigger: {
+        trigger: '.section.made-with',
+        start: 'top 60%',
+    }});
+
+    madeWithTimeline.from(
+        '.section.made-with .love .animate', 0.5,
         {
-            scrollTrigger: {
-                trigger: '.section.education .section-header',
-                start: 'top 80%',
-            },
             opacity: 0,
-            x: 100,
-            delay: 0.25,
-            stagger: { amount: 0.5 }
+            x: 50
         }
-    );
+    )
 
-    gsap.from(
-        '.section.education .line', 1.0,
+    madeWithTimeline.from(
+        '.section.made-with .gsap .animate', 0.5,
         {
-            scrollTrigger: {
-                trigger: '#education-start',
-                start: 'top 20%',
-            },
-            delay: 0.25,
-            height: '0%',
+            opacity: 0,
+            x: -50
         }
-    );
+    )
 
-    gsap.from(
-        ".section.about-me .container", 1.0,
+    madeWithTimeline.from(
+        '.section.made-with .lenis .animate', 0.5,
         {
-            scrollTrigger: {
-                trigger: '#about-me-start',
-                start: 'top 40%'
-            },
-            delay: 0.1,
-            opacity: 0
+            opacity: 0,
+            y: 50
         }
-    );
+    )
 }
 
 function applyExperienceAnimation()
